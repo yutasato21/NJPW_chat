7fbcbcfa01d90aad49608ec2957540a2e2b3944c
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-10 21:49:01 +0900

    モデル単体テストコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials(number: 3) }
+    email                 { Faker::Internet.free_email }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+  end
+end

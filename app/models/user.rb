58bc86415010b572cc73bfbe1fd8bad6c7618e08
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-11 17:13:17 +0900

    room_user中間テーブル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true, message: 'には英字と数字の両方を含めて設定してください'
 
-  has_many :rooms
+  has_many :room_users
+  has_many :room, through: :room_users
 end

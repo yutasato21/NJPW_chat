36ef8a03123694ffb8046cbc46a9b4a10b4dd0ea
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-11 18:08:05 +0900

    rooms#createアクション定義

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -6,4 +6,19 @@ class RoomsController < ApplicationController
   def new
     @room = Room.new
   end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, :text).merge(user_id: current_user.id)
+  end
 end

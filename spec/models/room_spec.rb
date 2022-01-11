aedbda90017291e046a7e59d1c5e4282ca6d0f1c
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-11 22:55:48 +0900

    Room単体モデルテストコード記述

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -7,7 +7,28 @@ RSpec.describe Room, type: :model do
 
   describe 'ルーム作成' do
     context 'ルーム作成できる場合' do
-      it 'name、text、user_idが存在していれば作成できる'
+      it 'name、text、user_idが存在していれば作成できる' do
+        expect(@room).to be_valid
+      end
+
+      it 'textが空でも作成できる' do
+        @room.text = ''
+        expect(@room).to be_valid
+      end
+    end
+
+    context 'ルーム作成できない場合' do
+      it 'nameが空では作成できない' do
+        @room.name = ''
+        @room.valid?
+        expect(@room.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'userが紐づいていないと作成できない' do
+        @room.user = nil
+        @room.valid?
+        expect(@room.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end

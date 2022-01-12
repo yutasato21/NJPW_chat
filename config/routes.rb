7a23c9779de063a35f7e8dc76d4ad473a6252d99
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-12 12:41:21 +0900

    users#edit,updateルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'rooms/index'
   root 'rooms#index'
 
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
   resources :rooms, only: [:index, :new, :create]
 end

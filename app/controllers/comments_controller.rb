f49d7b993bdccd72e3675943f1fa3e5b9d7b6c1f
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-17 11:54:12 +0900

    rooms#showからcomments#indexに仕様変更

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
 class CommentsController < ApplicationController
   def index
-    
+    @room = Room.find(params[:room_id])
+    @comments = @room.comments.includes(:user)
+    @comment = Comment.new
   end
 
   def create

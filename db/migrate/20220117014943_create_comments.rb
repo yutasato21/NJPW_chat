eb0bbac00efb62fc64f329bd4074e0c9982347fd
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-17 11:16:44 +0900

    マイグレーションファイル修正

--- db/migrate/20220117014943_create_comments.rb
+++ db/migrate/20220117014943_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string     :content
-      t.references :user
-      t.references :room
+      t.string     :content, null: false
+      t.references :user,    null: false, foreign_key: true
+      t.references :room,    null: false, foreign_key: true
       t.timestamps
     end
   end

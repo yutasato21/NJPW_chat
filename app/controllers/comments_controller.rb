f901a94f9d9a6e1067b8540360fd55d0824a7369
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-19 21:57:09 +0900

    comments#destroyルーティング設定

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,10 +10,11 @@ class CommentsController < ApplicationController
     end
   end
 
-  #def destroy
-    #@comment.destroy
-    #redirect_to root_path
-  #end
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to root_path
+  end
 
   private
 

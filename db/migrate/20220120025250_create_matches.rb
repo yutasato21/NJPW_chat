0400a6598713a1f173664712308bf0dac24dbd29
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-20 12:06:06 +0900

    Matchモデル、マイグレーションファイル作成

---
+++ db/migrate/20220120025250_create_matches.rb
@@ -0,0 +1,17 @@
+class CreateMatches < ActiveRecord::Migration[6.0]
+  def change
+    create_table :matches do |t|
+      t.string     :team_a_player,  null: false
+      t.string     :team_a_player2
+      t.string     :team_a_player3
+      t.string     :team_a_player4
+      t.string     :team_a_player5
+      t.string     :team_b_player,  null: false
+      t.string     :team_b_player2
+      t.string     :team_b_player3
+      t.string     :team_b_player4
+      t.string     :team_b_player5
+      t.timestamps
+    end
+  end
+end

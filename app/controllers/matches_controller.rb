f2af2189021d9bc92b6311a5c0c86f52d2621297
yutasato21 <yuta0201.t.chan@icloud.com>
2022-01-20 21:38:58 +0900

    ルーム作成者以外はカード作成画面に遷移できない処理を記述

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,11 +1,12 @@
 class MatchesController < ApplicationController
+  before_action :set_room, only: [:new, :create]
+  before_action :move_to_index, only: :new
+
   def new
-    @room = Room.find(params[:room_id])
     @match = Match.new
   end
 
   def create
-    @room = Room.find(params[:room_id])
     @match = @room.matches.new(match_params)
    if @match.save
      redirect_to room_path(params[:room_id])
@@ -18,5 +19,13 @@ class MatchesController < ApplicationController
 
   def match_params
     params.require(:match).permit(:game_number, :team_a_player, :team_a_player2, :team_a_player3, :team_a_player4, :team_a_player5, :team_b_player, :team_b_player2, :team_b_player3, :team_b_player4, :team_b_player5)
+  end
+
+  def set_room
+    @room = Room.find(params[:room_id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless current_user.id == @room.user_id
   end
 end

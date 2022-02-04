c20bbcc328923be92fd03e3f28a1e5d5fb66e69c
yutasato21 <yuta0201.t.chan@icloud.com>
2022-02-05 01:04:33 +0900

    commentsコントローラー不要記述削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,6 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to room_path(params[:room_id])
   end
